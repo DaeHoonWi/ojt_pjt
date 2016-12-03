ddc91b5e87507b881704ab24f904bdaabbce5127
회사 노트북 <daehan8789@naver.com>
2016-12-03 15:03:28 +0900

    .SQL - TABLE생성 코드, 제약조건 생성 코드 COMMIT

--- sql/ojt_pjt.sql
+++ sql/ojt_pjt.sql
@@ -1,5 +1,77 @@
-create user scott identified by tiger;
+CREATE TABLE CINEMA
+(
+	c_num                NUMBER PRIMARY KEY ,
+  cou_num              NUMBER NOT NULL ,
+	c_title              VARCHAR2(200) NOT NULL ,
+	c_runningtime        NUMBER NULL ,
+	c_summary            BLOB NULL ,
+	c_count              NUMBER NOT NULL ,
+	c_age                VARCHAR2(20) NOT NULL ,
+	c_original_file_name VARCHAR2(100) NULL ,
+	c_stored_file_name   VARCHAR2(200) NULL 
+);
 
-grant connect, resource to scott;
+--CINEMA END
 
-commit;
+
+CREATE TABLE COUNTRY
+(
+  cou_num              NUMBER PRIMARY KEY,
+	cou_name             VARCHAR2(30) NOT NULL
+);
+
+--COUNTRY END
+
+CREATE TABLE ACTOR
+(
+  ac_num               NUMBER PRIMARY KEY,
+	a_chracter           VARCHAR2(50) NOT NULL ,
+	c_num                NUMBER NOT NULL ,
+	a_part               VARCHAR2(20) NOT NULL ,
+	ac_code              NUMBER NOT NULL 
+);
+
+--ACTOR END
+
+CREATE TABLE ACTOR_CATEGORY
+(
+	ac_code              NUMBER PRIMARY KEY ,
+	ac_name              VARCHAR2(50) NOT NULL 
+);
+
+--ACTOR_CATEGORY END
+
+
+CREATE TABLE DIRECTOR
+(
+  d_num                NUMBER PRIMARY KEY ,
+	c_num                NUMBER NOT NULL ,
+	dc_code              NUMBER NOT NULL 
+);
+
+--DIRECTOR END
+
+CREATE TABLE DIRECTOR_CATEGORY
+(
+	dc_code              NUMBER PRIMARY KEY ,
+	dc_name              VARCHAR2(50) NOT NULL 
+);
+
+--DIRECTOR_CATEGORY END
+
+CREATE TABLE GENRE
+(
+  g_num                NUMBER PRIMARY KEY ,
+	c_num                NUMBER NOT NULL ,
+	gc_code              NUMBER NOT NULL
+);
+
+--GENRE END
+
+CREATE TABLE GENRE_CATEGORY
+(
+  gc_code              NUMBER PRIMARY KEY ,
+	gc_name              VARCHAR2(20) NOT NULL 
+);
+
+--GENRE_CATEGORY END
